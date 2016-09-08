f5f4f21b0fbdc9890313395e20234ca9ddf9a54f
Fernando Mendoza <fernandoza@gmail.com>
2016-09-08 09:49:51 -0500

    Initial commit for the java labs

---
+++ src/Lab9/Circle.java
@@ -0,0 +1,35 @@
+package Lab9;
+
+/**
+ * Created by Fer on 9/7/16.
+ */
+public class Circle {
+    public Circle(double radius){
+
+    }
+
+    public double getCircumference(){
+        return 1;
+    }
+
+    public String getFormattedCircumference(){
+        return "";
+    }
+
+    public double getArea(){
+        return 1;
+    }
+
+    public String getFormattedArea(){
+        return "";
+    }
+
+    private String formatNumber(double x){
+        return "";
+    }
+
+    public static int getObjectCount(){
+        return 1;
+    }
+
+}
---
+++ src/Lab9/CircleApp.java
@@ -0,0 +1,18 @@
+package Lab9;
+
+import java.util.Scanner;
+
+/**
+ * Created by Fer on 9/7/16.
+ */
+public class CircleApp {
+
+    public static void main(String[] args){
+        Circle cr = new Circle;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give me the radius: ");
+        int radius = sc.nextInt();
+        cr.getArea(radius);
+
+    }
+}
